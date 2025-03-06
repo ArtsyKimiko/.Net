fe135a0a5678a63cd3192ed78f9c66a21c0ac990
ArtsyKimiko <114069062+ArtsyKimiko@users.noreply.github.com>
2025-03-06 18:00:42 +0100

    Update Program.cs

--- Lab0/Program.cs
+++ Lab0/Program.cs
@@ -4,7 +4,42 @@
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+            FizzBuzz test = new FizzBuzz(20);
+            test.PrintFizzBuzz();
         }
+    }
+
+    class FizzBuzz
+    {
+        private int number;
+        public FizzBuzz(int maxNumber)
+        {
+            number = maxNumber;
+        }
+
+        public void PrintFizzBuzz()
+        {
+            string toPrint;
+            for (int i = 1; i <= number; i++)
+            {
+                if (i % 3 == 0 && i % 5 == 0)
+                {
+                    toPrint = "FizzBuzz";
+                }
+                else if (i % 3 == 0)
+                {
+                    toPrint = "Fizz";
+                }
+                else if (i % 5 == 0)
+                {
+                    toPrint = "Buzz";
+                }
+                else
+                {
+                    toPrint = i.ToString();
+                }
+                Console.WriteLine(toPrint);
+            }
+        }   
     }
 }
